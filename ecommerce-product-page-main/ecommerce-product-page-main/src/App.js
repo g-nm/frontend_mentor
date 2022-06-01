95efab9f87ce3357705e82a794ce346114bc2a8c
g-nm <gmang2236@gmail.com>
2022-06-01 10:26:02 +0300

    App Component and Styles

--- ecommerce-product-page-main/ecommerce-product-page-main/src/App.js
+++ ecommerce-product-page-main/ecommerce-product-page-main/src/App.js
@@ -1,7 +1,17 @@
 import './App.css';
+import Header from './Components/Header/Header';
+import Navbar from './Components/Navbar/Navbar';
+import ContextProvider from './Store/Context';
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="">
+      <ContextProvider>
+        <Navbar />
+        <Header />
+      </ContextProvider>
+    </div>
+  );
 }
 
 export default App;

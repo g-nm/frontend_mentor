d7be4d3d4598bd73295c84e75f978985574d3189
g-nm <gmang2236@gmail.com>
2022-06-02 10:27:50 +0300

    Created Data file to handle all products details

---
+++ ecommerce-product-page-main/ecommerce-product-page-main/src/data.js
@@ -0,0 +1,36 @@
+import productPicture1 from './images/image-product-1.jpg';
+import productPicture2 from './images/image-product-2.jpg';
+import productPicture3 from './images/image-product-3.jpg';
+import productPicture4 from './images/image-product-4.jpg';
+import productThumbnail1 from './images/image-product-1-thumbnail.jpg';
+import productThumbnail2 from './images/image-product-2-thumbnail.jpg';
+import productThumbnail3 from './images/image-product-3-thumbnail.jpg';
+import productThumbnail4 from './images/image-product-4-thumbnail.jpg';
+
+const data = [
+  {
+    id: 1,
+    productImage: productPicture1,
+    thumbnail: productThumbnail1,
+    alt: 'a pair of sneakers',
+  },
+  {
+    id: 2,
+    productImage: productPicture2,
+    thumbnail: productThumbnail2,
+    alt: 'a pair of sneakers with one shoe balancing on a rock',
+  },
+  {
+    id: 3,
+    productImage: productPicture3,
+    thumbnail: productThumbnail3,
+    alt: "a sneaker's shoe balancing on a rock",
+  },
+  {
+    id: 4,
+    productImage: productPicture4,
+    thumbnail: productThumbnail4,
+    alt: "a sneaker's shoe balancing on a rock",
+  },
+];
+export default data;

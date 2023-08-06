c06c45deaec5f17999f6933a2b85a12ca7515f16
gmwaniki <georgenmwaniki@gmail.com>
2023-08-06 20:37:20 +0200

    feature: add custom colors to tailwind config

--- newsletter/tailwind.config.js
+++ newsletter/tailwind.config.js
@@ -7,6 +7,12 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      white: 'hsl(0, 0%, 100%)',
+      grey: 'hsl(231, 7%, 60%)',
+      charcoalGrey: 'hsl(235, 18%, 26%)',
+      darkGrey: 'hsl(234, 29%, 20%)',
+    },
   },
   plugins: [],
 };

5b7e60c20d4b0cbd11d3860ce4c7d3d5d5ec7fd9
gmwaniki <georgenmwaniki@gmail.com>
2023-08-06 20:46:23 +0200

    feat: initial page setup

--- newsletter/src/app/page.tsx
+++ newsletter/src/app/page.tsx
@@ -1,7 +1,47 @@
 import React from 'react';
+import Image from 'next/image';
+import Text from './components/Text';
+import backgroundImageMobile from './images/illustration-sign-up-mobile.svg';
+import ListItem from './components/ListItem';
+import Button from './components/Button';
+import SectionTitle from './components/SectionTitle';
 
 export default function Page() {
   return (
-    <h1 className="text-4xl">Whereas recognition of the inherent dignity</h1>
+    <div>
+      <section>
+        <Image
+          src={backgroundImageMobile}
+          alt="Picture of dashboard with a graph and computer windows"
+          className="w-full"
+        />
+      </section>
+      <section className="px-3 pt-7">
+        <SectionTitle>Stay updated!</SectionTitle>
+        <Text className="pb-5">
+          Join 60,000+ product managers receiving monthly updates on:
+        </Text>
+        <ul>
+          <ListItem text="Product discovery and building what matters" />
+          <ListItem text="Measuring to ensure updates are a success" />
+          <ListItem text="And much more!" />
+        </ul>
+        <form className="pt-5">
+          <div className="flex flex-col gap-y-2">
+            <label htmlFor="email" className="text-sm font-bold">
+              Email address
+            </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="email@company.com"
+              className="border border-grey py-3 px-5 rounded-md"
+            />
+          </div>
+          <Button>Subscribe to monthly newsletter</Button>
+        </form>
+      </section>
+    </div>
   );
 }
